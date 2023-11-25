f35134c40e176da02abca0b1f3e3e5cbea590b7f
Dominik Vach <vach.dominik@gmail.com>
2023-11-25 22:16:11 +0100

    small updates to the example

--- examples/server_api_example.py
+++ examples/server_api_example.py
@@ -1,3 +1,4 @@
+from auth0_easy_api import generate_flask_callback_app
 
 domain="xxxxxxx.eu.auth0.com"
 client_id="yrkFiXhdjkljkljkjklgwYPLRHH"
@@ -5,15 +6,11 @@ client_secret = "u074UGaOmZsfjkhkjahvlkjhdFVf4p08jjyjvwI3-gPEs0sXY52"
 audience="https://xxxxxxx.eu.auth0.com/api/v2/"
 
 
-
-import auth0_easy_api as aea
-
-
 def store_user_function(user):
     with open("user_data.txt","w+") as file:
         file.write(str(user))
         
-app=aea.generate_flask_callback_app(domain, client_id, client_secret, audience, store_user_function=store_user_function)
+app=generate_flask_callback_app(domain, client_id, client_secret, audience, store_user_function=store_user_function)
 app.run()
 
 
