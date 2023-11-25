c10df8b8c6b22c1ee088e6229084d906b90496bf
Dominik Vach <vach.dominik@gmail.com>
2023-11-25 22:26:37 +0100

    initialize package 1.0.0

---
+++ auth0_easy_api/__init__.py
@@ -0,0 +1,3 @@
+from auth0_easy_api import *
+
+__all__=["auth0_easy_api", "examples"]
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='auth0_easy_api',
+    version='1.0.0',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='A Python wrapper around Auth0 Authentication API and Management API to make setup and basic operations easier',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DovaX/auth0_easy_api',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'flask','pyjwt','dogui'
+     ],
+    python_requires='>=3.6',
+)
+    
