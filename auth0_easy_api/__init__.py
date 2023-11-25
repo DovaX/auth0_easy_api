70be2cef68780d4a8a79ddb2ebe5bbb67e35ec5f
Dominik Vach <vach.dominik@gmail.com>
2023-11-25 23:18:26 +0100

    fix __all__ + release 1.0.3

--- auth0_easy_api/__init__.py
+++ auth0_easy_api/__init__.py
@@ -1,3 +1,3 @@
 from auth0_easy_api import *
-from auth0_easy_api_core import generate_flask_callback_app, open_authentication_page
+from auth0_easy_api.auth0_easy_api_core import generate_flask_callback_app, open_authentication_page
 __all__=["auth0_easy_api_core", "examples", "generate_flask_callback_app", "open_authentication_page"]
